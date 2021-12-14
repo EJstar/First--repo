ea47a2b64824e88d1fa96abdb99fed7d413ceefe
Phinehas Enakireru <phinehasenakireru@gmail.com>
2021-12-14 11:36:55 +0100

    Subtract functionality commit from Phinehas-1

--- public/Calculator.java
+++ public/Calculator.java
@@ -4,6 +4,12 @@ public void add(){
    int b = 3;
   int result = a + b;
 }
+
+public void subtract(){
+	int a = 5;
+	int b = 2;
+	int result = a -b;
+}
 public static void main(String[] args){
 
 }
